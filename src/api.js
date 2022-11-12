41502b3260c68db90abf33c6f1f943d95f3a3231
Animal Studio <61629960+AnimalStudioOfficial@users.noreply.github.com>
2022-11-11 20:35:12 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -23,63 +23,63 @@ router.get('/', (req, res, next) => {
   }
 });
 
-router.get('/posts', async (req, res, next) => {
-  try {
-    const posts = await Post.find().exec();
-    return res.status(200).json(posts);
-  } catch (err) {
-    next(err);
-  }
-});
+//router.get('/posts', async (req, res, next) => {
+//  try {
+ //   const posts = await Post.find().exec();
+ //   return res.status(200).json(posts);
+ // } catch (err) {
+ //   next(err);
+ // }
+//});
 
 router.get('/errorapi', async (req, res, next) => {
     const posts = "hi"
     return res.status(200).json(posts);
 });
 
-router.get('/posts/:id', async (req, res, next) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (!post) {
-      return res.status(422).json({
-        errors: {
-          message: `No post with id ${req.params.id} found.`,
-        },
-      });
-    }
-    return res.status(200).json(post);
-  } catch (err) {
-    next(err);
-  }
-});
+//router.get('/posts/:id', async (req, res, next) => {
+//  try {
+//    const post = await Post.findById(req.params.id);
+//    if (!post) {
+//      return res.status(422).json({
+//        errors: {
+//          message: `No post with id ${req.params.id} found.`,
+//        },
+//      });
+//    }
+//    return res.status(200).json(post);
+//  } catch (err) {
+//    next(err);
+//  }
+//});
 
-router.post('/posts', async (req, res, next) => {
-  try {
-    const createdPost = await Post.create(req.body);
-    return res.status(201).json(createdPost);
-  } catch (err) {
-    next(err);
-  }
-});
+//router.post('/posts', async (req, res, next) => {
+//  try {
+//    const createdPost = await Post.create(req.body);
+//    return res.status(201).json(createdPost);
+//  } catch (err) {
+//    next(err);
+//  }
+//});
 
-router.delete('/posts/:id', async (req, res, next) => {
-  try {
-    const postId = req.params.id;
-    const deletedPost = await Post.findByIdAndDelete(postId, {
-      useFindAndModify: false,
-    });
-    if (!deletedPost) {
-      return res.status(422).send({
-        errors: {
-          message: `No post with id: ${postId} was found.`,
-        },
-      });
-    }
-    return res.status(200).send(deletedPost);
-  } catch (err) {
-    next(err);
-  }
-});
+//router.delete('/posts/:id', async (req, res, next) => {
+//  try {
+ //   const postId = req.params.id;
+ //   const deletedPost = await Post.findByIdAndDelete(postId, {
+ //     useFindAndModify: false,
+  //  });
+  //  if (!deletedPost) {
+   //   return res.status(422).send({
+  //     errors: {
+  //        message: `No post with id: ${postId} was found.`,
+  //      },
+  //    });
+  //  }
+  //  return res.status(200).send(deletedPost);
+ // } catch (err) {
+  //  next(err);
+ // }
+//});
 
 app.use('/.netlify/functions/api', router);
 
