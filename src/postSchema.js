17a6588fa9dd8d9bd5cb3090cb8fef88b3be1939
Animal Studio <61629960+AnimalStudioOfficial@users.noreply.github.com>
2022-11-11 20:39:07 -0500

    Update postSchema.js

--- src/postSchema.js
+++ src/postSchema.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 
-const postSchema = mongoose.Schema(
-  {
-    title: { type: String, required: true },
-    body: { type: String, required: true },
-    author: { type: String, default: 'Unknown' },
-    categories: { type: Array, default: [] },
-    imgUrl: { type: String, default: null }
-  },
-  { collection: 'posts' }
-);
+//const postSchema = mongoose.Schema(
+//  {
+//    title: { type: String, required: true },
+//    body: { type: String, required: true },
+//    author: { type: String, default: 'Unknown' },
+//    categories: { type: Array, default: [] },
+//    imgUrl: { type: String, default: null }
+//  },
+//  { collection: 'posts' }
+//);
 
-module.exports = mongoose.model('Post', postSchema);
+//module.exports = mongoose.model('Post', postSchema);
